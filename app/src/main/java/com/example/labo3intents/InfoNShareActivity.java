f536770a06e3db639df1ba1cfeb73527ca300a8c
marielosagui <00078913@uca.edu.sv>
2019-03-22 17:01:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/labo3intents/InfoNShareActivity.java
@@ -0,0 +1,13 @@
+package com.example.labo3intents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoNShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_nshare);
+    }
+}
